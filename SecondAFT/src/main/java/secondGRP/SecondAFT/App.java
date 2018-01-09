30ff34a18fad8b7d5f3a76e73d7129032ede5c94
E001799 <E001799@CTLINDT-0879.cignitiglobal.com>
2018-01-09 19:04:10 +0530

    Second

---
+++ SecondAFT/src/main/java/secondGRP/SecondAFT/App.java
@@ -0,0 +1,13 @@
+package secondGRP.SecondAFT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
